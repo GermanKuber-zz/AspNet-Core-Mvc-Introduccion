424552177abb3dd3fa1947f807c3ede09d730fb9
Mariano Germán Villarreal Küber <Mariano Germn Villarreal Kber>
2016-10-08 17:47:27 -0300

    Se agrega archivo de configuracion

--- AdminNetBaires/Startup.cs
+++ AdminNetBaires/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,13 +14,18 @@ namespace AdminNetBaires
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        //TODO : Paso 1 - Agrego una variable donde guardar mi configuracion
+        public IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
+            //TODO : Paso 2 - Leo el archivo de configuracion
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
@@ -30,7 +37,9 @@ namespace AdminNetBaires
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //TODO : Paso 3 - Cargo la key de mi archivo de configuración
+                var saludo = Configuration["hello"];
+                await context.Response.WriteAsync(saludo);
             });
         }
     }
